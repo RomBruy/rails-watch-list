d69937662a344ff6e5c7b72de18343a011102711
romain bruyndonckx <romain.bruyndonckx@gmail.com>
2021-11-11 16:04:54 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#require 'json'
+#require 'open_uri'
+
+10.times do
+  url = "http://tmdb.lewagon.com/movie/top_rated"
+  movies = open(url).read
+  movie = JSON.parse(movies)
+  movie_results = movie['results']
+  movie_poster = "https://image.tmdb.org/t/p/w500"
+
+  movie_results.each do |movie|
+  new_movie = Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie_poster + movie['poster_path'], rating: movie['vote_average'])
+  puts "#{new_movie.title} has been created!"
+  new_movie.save!
+  end
+end
